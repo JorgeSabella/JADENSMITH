cd3970b12f88c0acacaf03f974e04c16ea6fdb0d
Ariel Mendez <arielmendez735@gmail.com>
2018-11-03 16:55:53 -0600

    Change navbar style from bootsrap to materialize

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className = "navbar navbar-dark bg-primary">
-            <a className = "navbar-brand" href="#">Jaden Smith</a>
-            <div className = "">
-                <ul className="navbar-nav">
+        <nav className = "nav-wrapper blue darken-3">
+            <div className = "container">
+                <a className = "brand-logo left" href="#">Jaden Smith</a>
+                <ul className="right">
                     <li className ="nav-item"><Link to="/" className="text-white">Home</Link></li>
                     <li><NavLink to="/exams" className="text-white">Exams</NavLink></li>
                     <li><NavLink to="/newexam" className="text-white">Create Exam</NavLink></li>
