f1662573c2a11f739ecc4b40eb0119bac8748f32
Ariel Mendez <arielmendez735@gmail.com>
2018-11-06 11:32:38 -0600

    Add image to side navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,20 +4,27 @@ import { Link, NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (
         <div>
-            <nav className="nav-wrapper blue darken-3">
+            <nav className="top-nav blue darken-3">
                 <div className="container">
-                    <a className="brand-logo center" href="#">Jaden Smith</a>
-                    <a className="sidenav-trigger" href="#" data-target="slide-out">
-                        <i className="material-icons">menu</i>
-                    </a>
+                    <a className="brand-logo left" href="#">Jaden Smith</a>
                 </div>
+                <ul className="sidenav sidenav-fixed" id="slide-out">
+                    <li>
+                        <div className="user-view center">
+                            <div>
+                                <img className="circle" src="img/jaden smith.jpg"/>
+                            </div>
+                        </div>
+                    </li>
+                    <li><Link to="/" className="text-white">Home</Link></li>
+                    <li><NavLink to="/exams" className="text-white">Exams</NavLink></li>
+                    <li><NavLink to="/newexam" className="text-white">Create Exam</NavLink></li>
+                    <li><NavLink to="/question" className="text-white">New Question</NavLink></li>
+                </ul>
             </nav>
-            <ul className="sidenav sidenav-fixed" id="slide-out">
-                <li className ="nav-item"><Link to="/" className="text-white">Home</Link></li>
-                <li><NavLink to="/exams" className="text-white">Exams</NavLink></li>
-                <li><NavLink to="/newexam" className="text-white">Create Exam</NavLink></li>
-                <li><NavLink to="/question" className="text-white">New Question</NavLink></li>
-            </ul>
+            <a className="top-nav sidenav-trigger full hide-on-large-only" href="#" data-target="slide-out">
+                <i className="material-icons white-text">menu</i>
+            </a>
         </div>
     );
 }
