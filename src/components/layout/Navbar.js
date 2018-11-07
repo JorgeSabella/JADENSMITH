71fde18c8b4adc2a8fd5599ca0e6d208fb5355a0
Ariel Mendez <arielmendez735@gmail.com>
2018-11-06 19:42:52 -0600

    Reorder navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div>
             <nav className="top-nav blue darken-3">
-                <div className="container">
+                <div className="nav-wrapper">
                     <a className="brand-logo left" href="#">Jaden Smith</a>
                 </div>
-                <ul className="sidenav sidenav-fixed" id="slide-out">
-                    <li>
-                        <div className="user-view center">
-                            <div>
-                                <img className="circle" src="img/jaden smith.jpg"/>
-                            </div>
-                        </div>
-                    </li>
-                    <li><NavLink to="/" className="text-white"><i className="material-icons">home</i>Home</NavLink></li>
-                    <li><NavLink to="/exams" className="text-white"><i className="material-icons">folder</i>Exams</NavLink></li>
-                    <li><NavLink to="/newexam" className="text-white"><i className="material-icons">edit</i>Create Exam</NavLink></li>
-                    <li><NavLink to="/question" className="text-white"><i className="material-icons">add</i>New Question</NavLink></li>
-                </ul>
+                <a className="top-nav sidenav-trigger full hide-on-large-only" href="#" data-target="slide-out">
+                    <i className="material-icons white-text">menu</i>
+                </a>
             </nav>
-            <a className="top-nav sidenav-trigger full hide-on-large-only" href="#" data-target="slide-out">
-                <i className="material-icons white-text">menu</i>
-            </a>
+            <ul className="sidenav sidenav-fixed" id="slide-out">
+                <li>
+                    <div className="user-view center">
+                        <div>
+                            <img className="circle" src="img/jaden smith.jpg"/>
+                        </div>
+                    </div>
+                </li>
+                <li><NavLink to="/" className="text-white"><i className="material-icons">home</i>Home</NavLink></li>
+                <li><NavLink to="/exams" className="text-white"><i className="material-icons">folder</i>Exams</NavLink></li>
+                <li><NavLink to="/newexam" className="text-white"><i className="material-icons">edit</i>Create Exam</NavLink></li>
+                <li><NavLink to="/question" className="text-white"><i className="material-icons">add</i>New Question</NavLink></li>
+            </ul>
         </div>
     );
 }
