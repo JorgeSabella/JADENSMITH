3853cda8da410b74afb5dcb638d32f131aa3672c
Jorge Andres Sabella <jorge_andres1996@hotmail.com>
2018-11-07 23:16:51 -0600

    Edit Question

--- src/store/reducers/reducer_posts.js
+++ src/store/reducers/reducer_posts.js
@@ -1,10 +1,13 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, QUESTION_DATA } from '../actions';
 
 export default function(state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
+        case QUESTION_DATA:
+            console.log("action",action);
+            return action.payload;
         default:
             return state;
     }
