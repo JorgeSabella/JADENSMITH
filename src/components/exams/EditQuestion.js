fdf5de59197bd6b392b20ff1f3b8be9336940f8b
Jorge Andres Sabella <jorge_andres1996@hotmail.com>
2018-11-10 22:21:09 -0600

    Added new fields to text forms

--- src/components/exams/EditQuestion.js
+++ src/components/exams/EditQuestion.js
@@ -4,6 +4,14 @@ import { connect } from 'react-redux';
 
 
 class EditQuestion extends Component {
+    constructor(){
+        super()
+        this.state={
+            Choice:true,
+            Open:false,
+            TrueFalse: false
+        }
+    }
 
     renderTextField(field) {
         return (
@@ -36,6 +44,30 @@ class EditQuestion extends Component {
         );
     }
 
+    operation(){
+        if(document.getElementById("Dropdown").value == 1){
+            this.setState({
+                Choice:true,
+                Open:false,
+                TrueFalse: false
+            })
+        }
+        if(document.getElementById("Dropdown").value == 2){
+            this.setState({
+                Choice:false,
+                Open:true,
+                TrueFalse: false
+            })
+        }
+        if(document.getElementById("Dropdown").value == 3){
+            this.setState({
+                Choice:false,
+                Open:false,
+                TrueFalse: true
+            })
+        }
+    }
+
     // TODO onSubmit()
 
     render() {
@@ -55,12 +87,45 @@ class EditQuestion extends Component {
                                 <Field label="Tema" name="pregunta" component="textarea" type="text"/>
                             </div>
                         </div>
+                        <div>
+                            <label>Tipo de Pregunta</label>
+                            <select id="Dropdown" onChange={() => this.operation()} class="browser-default" >
+                                <option value="1">Opcion Multiples</option>
+                                <option value="2">Abiertas</option>
+                                <option value="3">Verdadero o Falsos</option>
+                            </select>
+                        </div>
                         <label>Respuestas</label>
-                        <Field label="op1" name="group" component={this.renderRadioField} type="text"/>
-                        <Field label="op2" name="group" component={this.renderRadioField} type="radio"/>
-                        <Field label="op3" name="group" component={this.renderRadioField} type="radio"/>
-                        <Field label="op4" name="group" component={this.renderRadioField} type="radio"/>
-                    
+                        {   this.state.Choice?
+                            <div>
+                                <Field label="Valor Minimo de Variable" name="Min" component={this.renderTextField} type="text"/>
+                                <Field label="Valor Maximo de Variable" name="Max" component={this.renderTextField} type="text"/>   
+                                <Field label="op1" name="group" component={this.renderRadioField} type="text"/>
+                                <Field label="op2" name="group" component={this.renderRadioField} type="radio"/>
+                                <Field label="op3" name="group" component={this.renderRadioField} type="radio"/>
+                                <Field label="op4" name="group" component={this.renderRadioField} type="radio"/>  
+                            </div>
+                            :null                          
+                        }
+
+                        {
+                            this.state.TrueFalse?
+                            <div class="switch">
+                                <label>
+                                Falso
+                                <input type="checkbox" />
+                                <span class="lever"></span>
+                                Verdadero
+                                </label>
+                            </div>
+                            :null
+                        }
+
+                        { this.state.Open?
+                            <Field label="Respuesta Correcta" name="Correcta" component={this.renderTextField} type="text"/>
+                            :null
+                        }
+
                     <button className="btn btn-primary" type = "submit">Guardar</button>
                 </form>
             </div>
