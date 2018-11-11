f17fbcd1406b2d2e9e8a85aa0631c52408d083eb
David Acevedo <david@acevedo.com>
2018-11-11 16:56:23 -0600

    Update src/components/testPDF/TestPDF.js

--- src/components/testPDF/TestPDF.js
+++ src/components/testPDF/TestPDF.js
@@ -6,15 +6,130 @@ import { examData } from '../../store/actions';
 
 class TestPDF extends Component {
     
-    /*state = {"subject":"Mate","name":"Parcial 1","institution":"Tec de Monterrey","professor":"Mejorado","text_fields":[{"key":"Nombre: ","body":null}],"questions":[{"name":"Factorizacion","text":"Factoriza el pedo ilv","tipo":1,"equation":"(2x + 4x^2)","answers":[{"correct":true,"tipo":1,"text":null,"equation":"2x(1+2x)"}]}]} */
-    
+    constructor(props) {
+        super(props);
+        this.createPDF = this.createPDF.bind(this);
+    }
     
     componentDidMount() {
         this.props.examData();
     }
+    
+    createPDF() {
+        function nextLine () {
+            if (y > 720) {
+                doc.addPage()
+                y = 72
+            } else {
+                y += jump
+            }
+        }
+        
+        function printHeader(exam) {
+            doc.text(x,y,exam.subject)
+            nextLine()
+            doc.text(x,y,exam.name)
+            nextLine()
+            doc.text(x,y,exam.institution)
+            nextLine()
+            doc.text(x,y,exam.professor)
+            nextLine()
+            
+            _.map(exam.text_fields, textField => {
+                doc.text(x,y,textField.key)
+                nextLine()
+            })
+            //doc.text(x,y,'Matricula:_____________       Nombre: ______________________________________')
+            //nextLine()
+            //doc.text(x,y,'Fecha:______________')
+            nextLine()
+            nextLine()
+            nextLine()
+        }
+        
+        function printQuestion(question) {
+            if (question.tipo == 1 || question.tipo == 2) {
+                doc.text(x,y,question.text + " " + question.equation)
+                nextLine()
+                _.map(question.answers, answer => {
+                    doc.text(x,y,"     a. " + answer.text + " " + answer.equation)
+                    nextLine()
+                })
+            } else if (question.tipo == 3) {
+                doc.text(x,y,question.text + " " + question.equation)
+                nextLine()
+                nextLine()
+                nextLine()
+                nextLine()
+            }
+        }
+        
+        var doc = new jsPDF('p','pt','letter') //orientation (portrait), unit for coordinates (pt points), format/size of first page (letter)
+        //Letter size: 612 × 792 points
+        // 1 inch margin = 72 points
+        var x = 72
+        var y = 72
+        var font_size = 12
+        var line_spacing = 1.5
+        var jump = font_size * line_spacing
+        doc.setFont('times')
+        doc.setFontSize(font_size)
+        
+        //For each exam
+        _.map(this.props.exams, exam => {
+            
+            printHeader(exam)
+            
+            //For each question in the exam
+            _.map(exam.questions, question => {
+                printQuestion(question)
+            })
+            
+            
+            //Add page and start at top for next exam to print
+            doc.addPage()
+            y = 72
+        })
+        
+        //HEADER
+        //printHeader()
+
+        //EJEMPLO PREGUNTA OPCION MULTIPLE
+        //printMultipleChoice(0)
+
+        //EJEMPLO PREGUNTA ABIERTA
+        //printOpen(0)
+
+        //EJEMPLO PREGUNTA V/F
+        //printTrueFalse(0)
+
+
+        var i;
+        for (i = 0; i < 40; i++) { 
+            //printMultipleChoice(i)
+        }
+
+
+
+        doc.save('test.pdf')
+        
+    }
+    
+    
+    //just for testing, must be deleted when no longer necessary
+    renderExamsInfo() {
+        return _.map(this.props.exams, exam => {
+            return (
+                <div key={exam.id}>
+                {exam.subject}
+                {exam.name}
+                {exam.institution}
+                </div>
+            )
+        })
+    }
 
     render() {
-        const { exams } = this.props;
         /*return _.map(questions.undefined, question => {
             return (
                 <div className="question" key={question.name}>
@@ -22,11 +137,16 @@ class TestPDF extends Component {
                 </div>
             )
         })*/
+        const { exams } = this.props;
         return (
             <div className="container">
-                    <button onClick={createPDF} className="btn btn-primary" >Guardar</button>
+            {this.renderExamsInfo()}
+                    <button onClick={this.createPDF} className="btn btn-primary" >Guardar</button>
             </div>
-        );
+        )
+            /*<div className="container">
+                    <button onClick={this.createPDF} className="btn btn-primary" >Guardar</button>
+            </div>*/
         /*return _.map(examData, post=> {
         return (
             <div className="container">
@@ -42,7 +162,8 @@ class TestPDF extends Component {
 
 
 
-/////////////////////////////////////HI PDF////////////////////////////////////
+//PDFTEST
+/*
 var doc = new jsPDF('p','pt','letter') //orientation (portrait), unit for coordinates (pt points), format/size of first page (letter)
 //Letter size: 612 × 792 points
 // 1 inch margin = 72 points
@@ -108,7 +229,7 @@ function printTrueFalse(number) {
     nextLine()
 }
 
-function createPDF () {
+function createPDF2 () {
 
     doc.setFont('times')
     doc.setFontSize(font_size);
@@ -136,9 +257,8 @@ function createPDF () {
     doc.save('test.pdf')
 }
 
+*/
 
-
-///////////////////////////////////////BYE PDF/////////////////////////////////
 
 function mapStateToProps(state) {
     return { 
