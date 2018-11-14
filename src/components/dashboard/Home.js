91c66ac196c622f972e5c4761f52b3eefa48d93c
Ed Ag <eduxx77@gmail.com>
2018-11-14 17:06:07 -0600

    Fix Home.js

--- src/components/dashboard/Home.js
+++ src/components/dashboard/Home.js
@@ -4,10 +4,10 @@ const Home = () => {
     return (
         <div className = "container">
             <div className="video-container">
-            <iframe width="560" height="315" src="https://www.youtube.com/watch?v=dIcgyj1F9ms" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/dIcgyj1F9ms" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
             <p className="flow-text">
-                Bienvenido a excelente sistema de punta revolucionario en el mundo de la generacion de Examenes, JADEN SMITH. Con esta applicaccion usted podra generar facilmente examenes aleatorios para grupos grande de individuos. Facil, sencillo y rapido, lo invitamos a utlizar nuestra plataforma y reportar cualquier problema a jorge_andres1996&#174hotmail.com
+                Bienvenido a excelente sistema de punta revolucionario en el mundo de la generacion de Examenes, JADEN SMITH. Con esta applicaccion usted podra generar facilmente examenes aleatorios para grupos grande de individuos. Facil, sencillo y rapido, lo invitamos a utlizar nuestra plataforma y reportar cualquier problema a jorge_andres1996@hotmail.com
             </p>
         </div>
     );
