39ce84f4bb1484a6749e04f10357a079f9804bd4
Jorge Andres Sabella <jorge_andres1996@hotmail.com>
2018-11-13 21:38:08 -0600

    Updates

---
+++ precache-manifest.c4c11448f6e85f528bb4485c463416c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db08950ada926230beb3",
+    "url": "/JADENSMITH/static/js/runtime~main.db08950a.js"
+  },
+  {
+    "revision": "dda6bf2119494ff26178",
+    "url": "/JADENSMITH/static/js/main.dda6bf21.chunk.js"
+  },
+  {
+    "revision": "6836ee2add24dd717ad0",
+    "url": "/JADENSMITH/static/js/1.6836ee2a.chunk.js"
+  },
+  {
+    "revision": "dda6bf2119494ff26178",
+    "url": "/JADENSMITH/static/css/main.a7f25ffc.chunk.css"
+  },
+  {
+    "revision": "fb5ba0d9eb39b80b588d5f6abec84c23",
+    "url": "/JADENSMITH/index.html"
+  }
+];
